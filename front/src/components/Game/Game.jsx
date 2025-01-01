b0f5407830db07876e9dd6657d346646f2be88f2
Raul Arca <uo270587@uniovi.es>
2025-01-01 13:48:55 +0100

    add styles to make a game shelf

--- front/src/components/Game/Game.jsx
+++ front/src/components/Game/Game.jsx
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from "react";
 import "./Game.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import logoSwitch from '../../assets/images/logo-switch.png'
+import logoDS from '../../assets/images/logo-ds.png'
 
-const Game = () => {
+const Game = ({ platform, title }) => {
   const [isAnimating, setIsAnimating] = useState(false); // Estado para iniciar y detener la animación
-  const [isHover, setIsHover] = useState(false); 
+  const [isHover, setIsHover] = useState(false);
   const [position, setPosition] = useState({}); // Estilos dinámicos
   const [positionini, setPositionini] = useState({}); // Estilos dinámicos
   const [isReturning, setIsReturning] = useState(false); // Estado para saber si estamos volviendo al grid
@@ -30,7 +33,7 @@ const Game = () => {
       const windowWidth = window.innerWidth;
       const windowHeight = window.innerHeight;
       setPositionini(rect)
-      const targetX = (windowWidth - rect.width) / 1.4; // Coordenada X centrada
+      const targetX = (windowWidth - rect.width) / 1.5; // Coordenada X centrada
       const targetY = (windowHeight - rect.height) / 3; // Coordenada Y centrada
 
       // Iniciar animación hacia el centro
@@ -52,7 +55,7 @@ const Game = () => {
       }, 0);
 
       setIsAnimating(true);
-    } 
+    }
     else {
       // Empezar la animación para volver al grid
       setIsReturning(true); // Activar el estado de retorno
@@ -86,6 +89,73 @@ const Game = () => {
     setIsAnimating(!isAnimating); // Cambiar el estado de animación
   };
 
+  const renderPlatformIcon = (platform) => {
+    switch (platform) {
+      case "ps":
+        return (
+          <div className={`front ${isAnimating ? "animated" : ""}`}>
+            <div className="platform ps">
+              <div className="icon ps">
+                <FontAwesomeIcon
+                  icon="fa-brands fa-playstation"
+                  size="xl"
+                  style={{ color: "#ffffff" }}
+                />
+              </div>
+              <div> PS</div>
+            </div>
+            <div className="title ps">{title}</div>
+          </div>
+        );
+      case "xbox":
+        return (
+          <div className={`front ${isAnimating ? "animated" : ""}`}>
+            <div className="platform xbox">
+              <div className="icon xbox">
+                <FontAwesomeIcon icon="fa-brands fa-xbox" size="" rotation={90} style={{color: "#ffffff",}} />
+              </div>
+              <div> XBOX</div>
+            </div>
+            <div className="title xbox">{title}</div>
+          </div>
+        );
+      case "pc":
+        return (
+          <div className={`front pc ${isAnimating ? "animated" : ""}`}>
+            <div className="platform pc">
+              <div className="icon pc">
+              <FontAwesomeIcon icon="fa-brands fa-steam" size="xl" />
+              </div>
+            </div>
+            <div className="title pc">{title}</div>
+          </div>
+        );
+        case "ds":
+        return (
+          <div className={`front ds ${isAnimating ? "animated" : ""}`}>
+            <div className="platform ds">
+              <div className="icon ds">
+              <img src={logoDS} alt="Logo" style={{ width: "auto", height: "100%",rotate: "90deg" }} />
+              </div>
+            </div>
+            <div className="title ds">{title}</div>
+          </div>
+        );
+      default:
+        return (
+          <div className={`front switch ${isAnimating ? "animated" : ""}`}>
+            <div className="platform switch">
+              <div className="icon switch">
+              <img src={logoSwitch} alt="Logo" style={{ width: "100%", height: "auto" }} />
+              </div>
+            </div>
+            <div className="title switch">{title}</div>
+          </div>
+        );
+    }
+  };
+
+
   return (
     <div className="game-container">
       <div
@@ -95,8 +165,9 @@ const Game = () => {
         style={position} // Aplicar estilos dinámicos
         onClick={handleAnimationToggle}
       >
-        <div className={`front ${isAnimating ? "animated" : ""}`}>front</div>
-        <div className={`left ${isAnimating ? "animated" : ""}`}>left</div>
+          {renderPlatformIcon(platform)}
+       
+        <div className={`left ${platform } ${isAnimating ? "animated" : ""}`}>left</div>
       </div>
     </div>
   );
